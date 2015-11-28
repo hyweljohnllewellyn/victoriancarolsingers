4a83bb5a0d0e74087294600de7be3093961a4467
hyweljohnllewellyn <hywel.llewellyn@gmail.com>
2015-11-28 18:45:14 +0000

    playpause

--- assets/js/audioplay.js
+++ assets/js/audioplay.js
@@ -1,6 +1,6 @@
 $("map[name=my_image] area").on('click', function () {
 var $this = $(this);
-var id = $this.attr('id');
+ var id = $this.attr('id').replace(/area/, '');
 
 $.each($('audio'), function () {
    this.pause();
@@ -9,9 +9,9 @@ $.each($('audio'), function () {
     $this.toggleClass('active');
     if($this.hasClass('active')){
         $this.text('pause');
-      $('audio')[id].play();
+      $('audio[id^="sound"]')[id-1].play();
     } else {
         $this.text('play');
-      $('audio')[id].play();          
+      $('audio[id^="sound"]')[id-1].pause();         
     }
 });
