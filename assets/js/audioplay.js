6dfe1da888458cd81337c5bfdbd2dea84a3f0125
hyweljohnllewellyn <hywel.llewellyn@gmail.com>
2015-11-28 18:17:14 +0000

    audioplay

---
+++ assets/js/audioplay.js
@@ -0,0 +1,17 @@
+$("map[name=my_image] area").on('click', function () {
+var $this = $(this);
+var $id = $this.attr('id');
+
+$.each($('audio'), function () {
+   this.pause();
+});
+
+    $this.toggleClass('active');
+    if($this.hasClass('active')){
+        $this.text('pause');
+        $id.play();
+    } else {
+        $this.text('play');
+        $id.pause();
+    }
+});
