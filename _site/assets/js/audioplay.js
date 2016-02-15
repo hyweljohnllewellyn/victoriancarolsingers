ab22dca66c76782b05255d70c5be6eda0cc12de5
sarallewelyn <sara.e.llewellyn@gmail.com>
2016-02-15 20:05:13 +0000

    site

---
+++ _site/assets/js/audioplay.js
@@ -0,0 +1,18 @@
+$("map[name=my_image] area").on('click', function () {
+//$("#my_image area").on('click', function () {
+var $this = $(this);
+ var id = $this.attr('id').replace(/area/, '');
+
+$.each($('audio'), function () {
+   this.pause();
+});
+
+    $this.toggleClass('active');
+    if($this.hasClass('active')){
+        $this.text('pause');
+      $('audio[id^="sound"]')[id-1].play();
+    } else {
+        $this.text('play');
+      $('audio[id^="sound"]')[id-1].pause();
+    }
+});
